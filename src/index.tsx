489a67c1b4a7ee1b6a8a0bc5edcb121be618a09c
Nadezhda Budarina <hopechka@gmail.com>
2022-08-19 15:35:39 +0300

    feat: 1.add tailwindcss 2.add react-router-dom 3.add index.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import { StrictMode } from 'react';
 
 import { App } from '@/App';
 
+import './index.css';
+
 const container = document.getElementById('app');
 const root = createRoot(container!);
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>,
 );
