5affbf57738672df4b250906937dff125c3d80f0
Nadezhda Budarina <hopechka@gmail.com>
2022-08-20 14:23:44 +0300

    refactor: add  postcss-nesting

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
+/* eslint-disable global-require */
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    'autoprefixer',
+    require('tailwindcss/nesting'),
+    require('tailwindcss'),
+    require('postcss-preset-env'),
+  ],
 };
