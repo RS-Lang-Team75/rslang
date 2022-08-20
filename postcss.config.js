e37dd5fe079192adbe197f3f4aa624e800da2c63
Nadezhda Budarina <hopechka@gmail.com>
2022-08-20 14:23:44 +0300

    refactor: add  postcss-nesting

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   plugins: [
     'autoprefixer',
-    'tailwindcss/nesting',
+    require('tailwindcss/nesting'),
     require('tailwindcss'),
     require('postcss-preset-env'),
   ],
