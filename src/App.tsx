90842279277efd5432ae39c2c23354b71c448d4a
Nadezhda Budarina <hopechka@gmail.com>
2022-08-20 14:26:27 +0300

    feat:1.add structure 2.add styles for nav 3.add burger-menu for mobile screen 4. add main.pcss file 5. add component Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { Route, Routes } from 'react-router-dom';
 
-import { Navigation } from './components/Navigation';
-import { MainPage } from './pages/MainPage';
+import { Navigation } from './components/Navigation/Navigation';
+import { BookPage } from './pages/BookPage/BookPage';
+import { DictionaryPage } from './pages/DictionaryPage/DictionaryPage';
+import { GamesPage } from './pages/GamesPage/GamesPage';
+import { MainPage } from './pages/MainPage/MainPage';
+import { StatisticPage } from './pages/StatisticPage/StatisticPage';
+import { TeamPage } from './pages/TeamPage/TeamPage';
+import { VideoPage } from './pages/VideoPage/VideoPage';
 
 export const App = (): JSX.Element =>  (
   <>
     <Navigation/>
     <Routes>
       <Route path='/' element={<MainPage/>} />
+      <Route path='/book' element={<BookPage/>} />
+      <Route path='/dictionary' element={<DictionaryPage/>} />
+      <Route path='/games' element={<GamesPage/>} />
+      <Route path='/statistic' element={<StatisticPage/>} />
+      <Route path='/team' element={<TeamPage/>} />
+      <Route path='/video' element={<VideoPage/>} />
     </Routes>
   </>
 
---
+++ src/pages/StatisticPage/StatisticPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function StatisticPage (){
+  return(
+    <h1>StatisticPage</h1>
+  );
+}
