b23fdacf0f92688fb9a6d17a0ee3c9cdd9736eae
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-08-21 15:00:51 +0300

    feat: add form data gathering into object

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 
 import EmailIcon from '../../assets/icons/email-icon.svg';
 import LockIcon from '../../assets/icons/lock-icon.svg';
@@ -6,10 +6,21 @@ import UserIcon from '../../assets/icons/user-icon.svg';
 
 import './LoginPage.pcss';
 import { Button } from '@/components/Button/Button';
+import { UserCredentials } from '@/types/userTypes';
 
 export function LoginPage (){
 
   const [isLoginMode, setIsLoginMode] = useState<boolean>(true);
+  const [details, setDetails] = useState<UserCredentials>({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const submitHandler = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(details);
+  };
 
   return(
     <main className="login">
@@ -21,35 +32,62 @@ export function LoginPage (){
         {!isLoginMode &&
         <div className="form__normal">
           <UserIcon />
-          <input id="user" className="form__input" type="text" name="user" placeholder="User Name" />
+          <input
+            id="name"
+            onChange={e => setDetails({ ...details, name: e.target.value })}
+            value={details.name}
+            className="form__input"
+            type="text"
+            name="name"
+            placeholder="User Name"
+          />
         </div>
         }
 
         <div className="form__normal">
           <EmailIcon />
-          <input id="email" className="form__input" type="email" name="email" placeholder="Email Address" />
+          <input id="email"
+            value={details.email}
+            onChange={e => setDetails({ ...details, email: e.target.value })}
+            className="form__input"
+            type="email"
+            name="email"
+            placeholder="Email Address"
+          />
         </div>
 
         <div className={isLoginMode ? 'form__last' : 'form__normal'}>
           <LockIcon />
-          <input className="form__input" type="password" name="password" id="password" placeholder="Password" />
+          <input className="form__input"
+            value={details.password}
+            onChange={e => setDetails({ ...details, password: e.target.value })}
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
         </div>
 
         {!isLoginMode &&
         <div className="form__last">
           <LockIcon />
-          <input className="form__input" type="password" name="password" id="password" placeholder="Confirm password" />
+          <input className="form__input"
+            type="password"
+            name="password"
+            id="password-confirm"
+            placeholder="Confirm password"
+          />
         </div>
         }
 
         <Button
           text={isLoginMode ? 'Login' : 'Create user'}
           classBtn="form__login-btn"
-          onClick={() => console.log('press check')}
+          onClick={submitHandler}
         />
         <div className="form__sign-container">
           <Button
-            text={isLoginMode ? 'Sign Up' : 'Login'}
+            text={isLoginMode ? 'Sign Up?' : 'Already have an account?'}
             onClick={() => setIsLoginMode(m => !m)}
             classBtn='form__sign-up-btn'/>
         </div>
