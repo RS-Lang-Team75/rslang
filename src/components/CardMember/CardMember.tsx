058e1c6484a2457b8677a50192413142bab8005d
Nadezhda Budarina <hopechka@gmail.com>
2022-08-21 16:56:47 +0300

    feat: add block about team

--- src/components/CardMember/CardMember.tsx
+++ src/components/CardMember/CardMember.tsx
@@ -1,9 +1,54 @@
 import React from 'react';
 
+import GitPNG from '../../assets/images/GitHub-Mark-32px.png';
+import TelegramSvg from '../../assets/svg/telegram-logo.svg';
 import './CardMember.pcss';
 
-export function CardMember () : JSX.Element{
+interface CardMemberProps {
+  imgLink:string;
+  memberName:string;
+  memberRole:string;
+  gitHubLink:string;
+  telegramSvg:string;
+}
+
+export function CardMember ({
+  imgLink,
+  memberName,
+  memberRole,
+  gitHubLink,
+  telegramSvg }:CardMemberProps) : JSX.Element{
   return (
-    <div />
+
+    <div className="card">
+      <div className='wrapperImg'>
+        <img className="cardImg" src={imgLink} alt={memberName}/>
+      </div>
+
+      <div className="cardBody">
+        <div className="cardMemberName">{memberName}</div>
+        <div className="cardMemberRole">{memberRole}</div>
+        <p className="cardText">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+       Voluptatibus quia, nulla! Maiores et perferendis eaque,
+       exercitationem praesentium nihil.
+        </p>
+      </div>
+      <div className="cardFooter">
+        <div className="link">
+          <a href={gitHubLink}
+            rel="noreferrer noopener"
+            target="_blank">
+            <img src={GitPNG} alt="GitHub link" />
+          </a></div>
+        <div className="link">
+          <a href={telegramSvg}
+            rel="noreferrer noopener"
+            aria-label = "Telegram-link"
+            target="_blank" ><TelegramSvg /></a>
+        </div>
+
+      </div>
+    </div>
   );
 }
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -5,12 +5,8 @@ import { AboutTeam } from '@/components/AboutTeam/AboutTeam';
 export function MainPage (){
 
   return(
-    <main className='flex-auto' >
-      <h1 className="text-3xl font-bold underline">
-      RSLang
-      </h1>
+    <main >
       <AboutTeam/>
-
     </main>
   );
 }
