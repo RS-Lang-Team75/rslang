b8a17abefed07c9a92bc5bd11026d974cae5718b
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-08-21 13:41:19 +0300

    feat: add button onClick property

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,8 +5,9 @@ import './Button.pcss';
 interface ButtonProps{
   text: string;
   classBtn : string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export const Button = ({ text,classBtn }:ButtonProps): JSX.Element => (
-  <button  type="button" className={classBtn} >{text}</button>
+export const Button = ({ text, classBtn, onClick }:ButtonProps): JSX.Element => (
+  <button type="button" className={classBtn} onClick={onClick}>{text}</button>
 );
