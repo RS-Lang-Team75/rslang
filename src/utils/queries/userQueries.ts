4b154886d57983d94b3b8e5426f51b921b5a7d41
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-08-22 23:25:52 +0300

    refactor: rearrange files

--- src/utils/queries/userQueries.ts
+++ src/utils/queries/userQueries.ts
@@ -2,6 +2,14 @@ import axios from 'axios';
 
 import { UserCredentials } from '@/types/userTypes';
 
+export interface UserResponse {
+  message: string;
+  token: string;
+  refreshToken: string;
+  userId: string;
+  name: string;
+}
+
 const axiosConfig = {
   headers: {
     'Accept': 'application/json',
@@ -9,16 +17,16 @@ const axiosConfig = {
   },
 };
 
-export const createUser = async (user: UserCredentials): Promise<UserCredentials> => {
-  const response = await axios.post<UserCredentials>(
+export const createUser = async (user: UserCredentials): Promise<UserResponse> => {
+  const response = await axios.post<UserResponse>(
     'https://rslang-team75.herokuapp.com/users',
     user,
     axiosConfig);
   return response.data;
 };
 
-export const signIn =async (user: UserCredentials): Promise<UserCredentials> => {
-  const response = await axios.post<UserCredentials>(
+export const signIn =async (user: UserCredentials): Promise<UserResponse> => {
+  const response = await axios.post<UserResponse>(
     'https://rslang-team75.herokuapp.com/signin',
     user,
     axiosConfig);
