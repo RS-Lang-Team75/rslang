74b523393258885777b7541222affc549fe9f42a
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-08-22 21:44:58 +0300

    feat: add query utils

--- src/utils/queryUtils.ts
+++ src/utils/queryUtils.ts
@@ -1,7 +1,26 @@
+import axios from 'axios';
+
 import { UserCredentials } from '@/types/userTypes';
 
-const createUser = (user: UserCredentials) => {
-  console.log(user);
+const axiosConfig = {
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
 };
 
-export default createUser;
+export const createUser = async (user: UserCredentials): Promise<UserCredentials> => {
+  const response = await axios.post<UserCredentials>(
+    'https://rslang-team75.herokuapp.com/users',
+    user,
+    axiosConfig);
+  return response.data;
+};
+
+export const signIn =async (user: UserCredentials): Promise<UserCredentials> => {
+  const response = await axios.post<UserCredentials>(
+    'https://rslang-team75.herokuapp.com/signin',
+    user,
+    axiosConfig);
+  return response.data;
+};
