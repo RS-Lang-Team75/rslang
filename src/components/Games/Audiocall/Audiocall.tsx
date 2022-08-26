f9a13dcfe0ef98670d473e30b4d50b6c2897bd5d
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-08-26 19:25:09 +0300

    feat: add generation of random answers

--- src/components/Games/Audiocall/Audiocall.tsx
+++ src/components/Games/Audiocall/Audiocall.tsx
@@ -26,11 +26,6 @@ export default function AudioCall (props: IAudioCall) {
 
   const user = useSelector((state: RootState) => state.user);
 
-  const returnRandomWords = async (page: number, group: number): Promise<void> => {
-    const randomWords = await randomWordsQuery(page, group);
-    setPageWords(randomWords);
-  };
-
   function shuffleArray<T> (arr: Array<T>): Array<T> {
     const shuffledArray = arr.slice();
     for (let i = 0; i < shuffledArray.length; i += 1) {
@@ -40,11 +35,27 @@ export default function AudioCall (props: IAudioCall) {
     return shuffledArray;
   }
 
-  const generateWordsForGame = () => {
+  const generateWordsForGame = (): void => {
     const shuffledPageWords = shuffleArray(pageWords);
     const randomWordsForGame = shuffledPageWords.slice(10);
     setWordsForGame(randomWordsForGame);
-    console.log(randomWordsForGame);
+  };
+
+  const returnRandomWords = async (page: number, group: number): Promise<void> => {
+    const randomWords = await randomWordsQuery(page, group);
+    setPageWords(randomWords);
+  };
+
+  const generateAnswers = (word: IWord): string[] => {
+    const answers = [word.wordTranslate];
+    while (answers.length < 5) {
+      const ind = Math.floor(Math.random() * pageWords.length);
+      if (pageWords[ind].wordTranslate !== word.wordTranslate) {
+        answers.push(pageWords[ind].wordTranslate);
+      }
+    }
+    console.log(answers);
+    return answers;
   };
 
   return(
@@ -52,9 +63,11 @@ export default function AudioCall (props: IAudioCall) {
       <h1>Audiocall</h1>
       <p>User: {user.name}</p>
       <p>ID: {user.userId}</p>
+      {pageWords.length === 0 &&
       <DifficultySelector
         returnRandomWords={returnRandomWords}
       />
+      }
       {pageWords.length > 0 &&
       <section className='gameSection'>
 
@@ -80,6 +93,13 @@ export default function AudioCall (props: IAudioCall) {
               playFirstOnly
             /></div>
           <div>{wordsForGame[shownWordNumber].wordTranslate}</div>
+          <div className='answerBtnContainer'>
+            {
+              shuffleArray(generateAnswers(wordsForGame[shownWordNumber])).map((w,i) =>
+                <div className='answerBtn'
+                  key={`${w.charCodeAt(0).toString(16)}${i*1}`}>{w}</div>)
+            }
+          </div>
         </div>
       }
       {pageWords.length > 0 && <div className='gameSection temp'>
