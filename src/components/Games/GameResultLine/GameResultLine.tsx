4b22f6cb476c1bd7bb1501e9cc479998775cac67
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-08-27 17:14:59 +0300

    feat: add results details

--- src/components/Games/GameResultLine/GameResultLine.tsx
+++ src/components/Games/GameResultLine/GameResultLine.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './GameResultLine.pcss';
+import { SoundButton } from '@/components/Button/SoundButton';
 import { IWord } from '@/types/types';
 
 interface IGameResultLine {
@@ -12,8 +13,15 @@ export function GameResultLine (props: IGameResultLine){
   const { word } = props;
 
   return(
-    <section>
-      <div>{word.word}</div>
+    <section className='resultLine'>
+      <SoundButton
+        word= {word}
+        classBtn='audioBtn'
+        playFirstOnly
+      />
+      <div className='word'>{word.word}</div>
+      <div className='dash'> - </div>
+      <div className='translation'>{word.wordTranslate}</div>
     </section>
   );
 }
