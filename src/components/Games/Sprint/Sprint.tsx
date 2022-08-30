5fdd6e4453676cd19fbb250fdd3377db008450dc
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-08-30 21:11:51 +0300

    feat: add initial wordQuery and finish stats

--- src/components/Games/Sprint/Sprint.tsx
+++ src/components/Games/Sprint/Sprint.tsx
@@ -18,13 +18,25 @@ import './Sprint.pcss';
 
 export default function Sprint () {
 
+  const [pageWords, setPageWords] = useState<IWord[]>([]);
+  const [correctAnswers, setCorrectAnswers] = useState<IWord[]>([]);
+  const [wrongAnswers, setWrongAnswers] = useState<IWord[]>([]);
+
+  const [isStartedFromBook] = useState<boolean>(pageWords.length > 0);
+  const [isGameStarted, setIsGameStarted] = useState<boolean>(false);
   const [isGameFinished, setIsGameFinished] = useState<boolean>(false);
 
   const user = useSelector((state: RootState) => state.user);
 
+  const returnRandomWords = async (page: number, group: number): Promise<void> => {
+    const randomWords = await getWordsQuery(page, group);
+    setPageWords(randomWords);
+  };
+
   const onTimerEnd = () => {
+    setIsGameStarted(false);
     setIsGameFinished(true);
-  }
+  };
 
   return(
     <main className='gamesPage'>
@@ -32,8 +44,33 @@ export default function Sprint () {
         <h2>Sprint</h2>
         <h2>User: {user.name}</h2>
         <h3>ID: {user.userId}</h3>
-        <Countdown onTimerEnd={onTimerEnd}/>
-        {isGameFinished && <p>OVER!</p> }
+        {!isGameStarted && !isGameFinished &&
+          <>
+            {!isStartedFromBook &&
+              <DifficultySelector
+                returnRandomWords={returnRandomWords} />
+            }
+            <Button text='Начать игру'
+              classBtn='nextRound'
+              onClick={() => {
+                if (pageWords.length > 0) {
+                  setIsGameStarted(true);
+                }}}
+            /></>}
+        {pageWords.length > 0 && isGameStarted &&
+          <Countdown onTimerEnd={onTimerEnd}/>
+        }
+        {isGameFinished && <section className='flex flex-col justify-center'>
+          <h2>Game is finished!</h2>
+          <GameResults correctAnswers={correctAnswers} wrongAnswers={wrongAnswers}/>
+          <Button text='Начать сначала'
+            classBtn='nextRound'
+            onClick={() => {
+              setIsGameFinished(false);
+              setCorrectAnswers([]);
+              setWrongAnswers([]);
+            }}/>
+        </section>}
       </div>
     </main>
   );
