a11f915fcad346e149d97a8a5531736bbe3b18dd
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-08-31 23:27:17 +0300

    fix: mobile navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -83,6 +83,7 @@ export function Navigation (): JSX.Element {
                 ['LOGIN', '/login'],
                 ['Учебник', '/book'],
                 ['Аудиовызов', '/audiocall'],
+                ['Спринт', '/sprint'],
                 ['Статистика', '/statistic'],
                 ['Команда', '/team'],
               ].map(([title, url]) => (
