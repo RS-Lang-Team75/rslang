2afe2cb667237a3a41869a1cce13e970149c9a3e
Nadezhda Budarina <hopechka@gmail.com>
2022-09-01 11:58:04 +0300

    feat:add charts for long statistic

--- src/pages/StatisticsPage/StatisticPage.tsx
+++ src/pages/StatisticsPage/StatisticPage.tsx
@@ -2,42 +2,62 @@
 import axios, { AxiosError, AxiosRequestConfig } from 'axios';
 import { useSelector } from 'react-redux';
 
+import { useEffect, useState } from 'react';
+
+import { ChartStatistic } from '@/components/ChartStatistic/ChartStatistic';
 import { UserStatistics } from '@/types/types';
 import { statisticsForStudiedWords } from '@/utils/queries/statisticQueries';
 import { RootState } from '@/utils/store/store';
 
 export function StatisticsPage (){
+  const userStatistics:UserStatistics = {
+    learnedWords:0,
+    optional:{
+      stateLearnedByDay:'[]',
+      stateNewWords:'[]',
+      stateLearnedLong:'[]',
+    },
+  };
+  const [statisticState, setStatisticState] = useState<UserStatistics>(userStatistics);
   const user = useSelector((state: RootState) => state.user);
   const SERVER_URL = 'https://rslang-team75.herokuapp.com';
 
-  const wordsAxiosConfig: AxiosRequestConfig = {
-    headers: {
-      'Authorization': `Bearer ${user.token}`,
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-  };
+  useEffect(()=>{
+    const wordsAxiosConfig: AxiosRequestConfig = {
+      headers: {
+        'Authorization': `Bearer ${user.token}`,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    };
 
-  const getStatistics = async () => {
-    try {
-      const response = await axios.get<UserStatistics>(
-        `${SERVER_URL}/users/${user.userId}/statistics`,
-        wordsAxiosConfig);
-      console.log('statistics data', response.data);
-      await statisticsForStudiedWords(user);
+    const getStatistics = async () => {
+      try {
+        await statisticsForStudiedWords(user);
 
-    } catch (e:unknown) {
-      const error = e as AxiosError;
-      console.log(error);
-    }
+        const response = await axios.get<UserStatistics>(
+          `${SERVER_URL}/users/${user.userId}/statistics`,
+          wordsAxiosConfig);
 
-  };
+        // console.log('statistics data', response.data);
+        setStatisticState(response.data);
+
+      } catch (e:unknown) {
+        const err = e as AxiosError;
+        throw new Error(`GET statisticsPage query error, ${err.message}`);
+      }
+
+    };
+    getStatistics();
+  },[user]);
+
   return(
     <main>
-      <button
-        type = 'button'
-        onClick={getStatistics}
-        className='w-30 h-10 bg-orange-400 border-red-400'>get statistics</button>
+      {!user.userId && <h1 className='message'>Статистика доступна только для авторизированных пользователей</h1>}
+      {user.userId && <div className='flex w-5/6 justify-center items-center mx-auto'>
+        <ChartStatistic statisticState = {statisticState.optional}/>
+      </div>}
+
     </main>
 
   );
