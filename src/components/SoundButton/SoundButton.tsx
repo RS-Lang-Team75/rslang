e63948a7f3bafb2ff1acb5546afc58caaa759e17
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-09-04 11:49:32 +0300

    fix: fix useEffect

--- src/components/SoundButton/SoundButton.tsx
+++ src/components/SoundButton/SoundButton.tsx
@@ -86,6 +86,7 @@ export function SoundButton (props: SoundButtonProps) {
         throw new Error('problem playing audio');
       });
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [word]);
 
   return (
