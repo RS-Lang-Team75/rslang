dedd8e46e3224dbf7140ff259b9973643ed9021b
Artem <36455467+a-selyugin@users.noreply.github.com>
2022-09-04 22:02:19 +0300

    fix: user refreshToken issue

--- src/utils/queries/userQueries.ts
+++ src/utils/queries/userQueries.ts
@@ -6,11 +6,15 @@ import { SERVER_URL } from './url';
 import { UserCredentials } from '@/types/userTypes';
 
 export interface UserResponse {
-  message: string;
   token: string;
   refreshToken: string;
   userId: string;
   name: string;
+}
+
+export interface UserTokenResponse {
+  token: string;
+  refreshToken: string;
 }
 
 export const createUser = async (user: UserCredentials): Promise<UserResponse> => {
@@ -26,5 +30,12 @@ export const signIn =async (user: UserCredentials): Promise<UserResponse> => {
     `${SERVER_URL}/signin`,
     user,
     setAxiosConfig());
+  return response.data;
+};
+
+export const getNewTokens = async (user: UserResponse): Promise<UserTokenResponse> => {
+  const response = await axios.get<UserResponse>(
+    `${SERVER_URL}/users/${user.userId}/tokens`,
+    setAxiosConfig(user.refreshToken));
   return response.data;
 };
